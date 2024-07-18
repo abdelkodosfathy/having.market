d77d34205a311668fc1f38bfd214bf65579875f2
abdelkodosfathy <abdelkodosfathy@gmail.com>
2024-07-18 19:06:17 +0300

    Updates

---
+++ assets/NotFound-BmPJ7332.js
@@ -0,0 +1,1 @@
+import{u as n,j as o}from"./index-kU5mV01F.js";/* empty css               */const i=()=>{let t=n();return o.jsxs("div",{className:"not-found",children:[o.jsx("h1",{children:"wrong link..."}),o.jsx("button",{onClick:()=>t("/"),children:"Go To Home?"})]})};export{i as default};
