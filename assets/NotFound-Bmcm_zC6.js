8ff7b11604d995d874507eaeb1e761efa8977c4e
abdelkodosfathy <abdelkodosfathy@gmail.com>
2024-07-18 19:10:26 +0300

    Updates

--- assets/NotFound-Bmcm_zC6.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as o}from"./index-wW8Djhe-.js";/* empty css               */const i=()=>{let t=n();return o.jsxs("div",{className:"not-found",children:[o.jsx("h1",{children:"wrong link..."}),o.jsx("button",{onClick:()=>t("/"),children:"Go To Home?"})]})};export{i as default};
